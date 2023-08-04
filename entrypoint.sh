86a1923f426bd6be86ec876fe922d1fcfd9e4f61
sulaiman-coder <51925710+sulaiman-coder@users.noreply.github.com>
2023-08-04 11:16:03 +0600

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+exit_err() {
+    echo -n "ERROR: "
+    echo $@
+    exit 1
+}
+
+uname -a | grep -q aarch64 && arch=aarch64
+uname -a | grep -q x86_64 && arch=x86_64
+
+cd /$arch
+
+what=$@
+
+for test in $@; do
+    t=$test
+
+    t=${t#"TRC-"}
+    t=${t#"trc-"}
+    t=${t#"TRC"}
+    t=${t#"trc"}
+    t=${t%".sh"}
+
+    file=trc${t}.sh
+
+    if [ ! -x $file ]; then
+        exit_err "could not find test $file"
+    fi
+
+    ./$file
+done
